844c3417d279d1f1061f36fad1d1d4e3fedfefff
Dinaka <sadariskar.d@gmail.com>
2021-06-10 12:30:24 +0700

    Initial Commit

---
+++ favorite-page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favorite-page
+//
+//  Created by Dinaka Sadariskar on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
