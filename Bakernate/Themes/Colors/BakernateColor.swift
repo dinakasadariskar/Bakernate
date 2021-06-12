fe7286a51b9206886ae3a4ece5298cfb804f0f21
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2021-06-13 00:28:00 +0700

    add color theme

---
+++ Bakernate/Themes/Colors/BakernateColor.swift
@@ -0,0 +1,24 @@
+//
+//  BakernateColor.swift
+//  Bakernate
+//
+//  Created by Roshani Ayu Pranasti on 12/06/21.
+//
+
+import UIKit
+
+struct BakernateColor {
+    let backgroundGreen = UIColor(named: "BackgroundGreen")
+    let green100 = UIColor(named: "Green100")
+    let green70 = UIColor(named: "Green70")
+    let green50 = UIColor(named: "Green50")
+    let green20 = UIColor(named: "Green20")
+    let green10 = UIColor(named: "Green10")
+    let brown100 = UIColor(named: "Brown100")
+    let brown70 = UIColor(named: "Brown70")
+    let brown50 = UIColor(named: "Brown50")
+    let brown20 = UIColor(named: "Brown20")
+    let brown10 = UIColor(named: "Brown10")
+    let black = UIColor(named: "Black")
+    let white = UIColor(named: "White")
+}
