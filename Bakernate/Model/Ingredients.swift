b1984d7903d6295c8bb7da13c10cd375a54334c8
Apriliani Putri Prasetyo <apepe96@gmail.com>
2021-06-13 12:32:19 +0700

    add coredata

---
+++ Bakernate/Model/Ingredients.swift
@@ -0,0 +1,24 @@
+//
+//  Ingredients.swift
+//  Bakernate
+//
+//  Created by Apriliani Putri Prasetyo on 13/06/21.
+//
+
+import Foundation
+
+struct Ingredients {
+    
+    var ingredientId = ""
+    var ingredientName = ""
+    var ingredientDesc = ""
+    var ingredientImage = ""
+    var isDairy = false
+    var isEgg = false
+    var isGluten = false
+    var isPeanut = false
+    var isSoy = false
+    var isTreeNuts = false
+    var isVegan = false
+    var isFavorited = false
+}
